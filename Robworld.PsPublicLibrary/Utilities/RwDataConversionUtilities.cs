3ecbc7122c171082702b50ad569b4c5e25e77a95
markush <markus.hochholzer@robworld.de>
2020-02-29 14:10:47 +0100

    Add Data conversion and Math utilities

---
+++ Robworld.PsPublicLibrary/Utilities/RwDataConversionUtilities.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace Robworld.PsPublicLibrary.Utilities
+{
+    /// <summary>
+    /// Methods for conversion of a data type to another data type
+    /// </summary>
+    public static class RwDataConversionUtilities
+    {
+        #region Fields
+        private static readonly NumberStyles doubleNumberStyle = NumberStyles.Float;
+        private static readonly CultureInfo culture = CultureInfo.InvariantCulture;
+        #endregion
+
+        /// <summary>
+        /// Convert a string to a double
+        /// </summary>
+        /// <param name="value">The string to be converted</param>
+        /// <param name="number">The variable where the converted value is stored</param>
+        /// <returns>The conversion result</returns>
+        public static bool ConvertToDouble(string value, out double number)
+        {
+            if(double.TryParse(value, doubleNumberStyle, culture, out number))
+            {
+                return true;
+            }
+            else
+            {
+                number = double.NaN;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Convert a string to an integer
+        /// </summary>
+        /// <param name="value">The string to be converted</param>
+        /// <param name="number">The variable where the converted value is stored</param>
+        /// <returns>The conversion result</returns>
+        public static bool ConvertToInteger(string value, out int number)
+        {
+            if (int.TryParse(value, NumberStyles.None, culture, out number))
+            {
+                return true;
+            }
+            else
+            {
+                number = 0;
+                return false;
+            }
+        }
+    }
+}
---
+++ Robworld.PsPublicLibrary/Utilities/RwMathUtilities.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Tecnomatix.Engineering;
+
+namespace Robworld.PsPublicLibrary.Utilities
+{
+    /// <summary>
+    /// Provides static mathematical methods 
+    /// </summary>
+    public static class RwMathUtilities
+    {
+        /// <summary>
+        /// Converts an angle fom radians to degrees
+        /// </summary>
+        /// <param name="radians">Value in radians</param>
+        /// <returns>The value in degrees</returns>
+        public static double Rad2Deg(double radians)
+        {
+            return (radians / Math.PI) * 180.0;
+        }
+
+        /// <summary>
+        /// Converts an angle from degrees to radians
+        /// </summary>
+        /// <param name="degrees">Value in degrees</param>
+        /// <returns>The value in radians</returns>
+        public static double Deg2Rad(double degrees)
+        {
+            return (degrees / 180.0) * Math.PI;
+        }
+
+        /// <summary>
+        /// Rounds a double value to the nearest integral
+        /// </summary>
+        /// <param name="value">The value that has to be rounded</param>
+        /// <returns></returns>
+        public static double Round(double value)
+        {
+            return Math.Round(value, 3);
+        }
+
+        /// <summary>
+        /// Create a TxTransformation object in RPY representation
+        /// </summary>
+        /// <param name="x">The x coordinate</param>
+        /// <param name="y">The y coordinate</param>
+        /// <param name="z">The z coordinate</param>
+        /// <param name="rx">The rx rotation</param>
+        /// <param name="ry">The ry rotation</param>
+        /// <param name="rz">The rz rotation</param>
+        /// <param name="convertToRadians">Convert the rotation values to radians</param>
+        /// <returns>The TxTransformation object</returns>
+        public static TxTransformation CreateRpyTransformation(double x, double y, double z, double rx, double ry, double rz, bool convertToRadians)
+        {
+            TxVector translation = CreateVector(x, y, z, false);
+            TxVector rotation = CreateVector(rx, ry, rz, true);
+            return new TxTransformation(translation, rotation, TxTransformation.TxRotationType.RPY_XYZ);
+        }
+
+        /// <summary>
+        /// Create a TxVector object
+        /// </summary>
+        /// <param name="v1">The first value</param>
+        /// <param name="v2">The second value</param>
+        /// <param name="v3">The third value</param>
+        /// <param name="convertToRadians">Convert the values to radians</param>
+        /// <returns></returns>
+        public static TxVector CreateVector(double v1, double v2, double v3, bool convertToRadians)
+        {
+            TxVector vector;
+            if(convertToRadians)
+            {
+                vector = new TxVector(Deg2Rad(v1), Deg2Rad(v2), Deg2Rad(v3));
+            }
+            else
+            {
+                vector = new TxVector(v1, v2, v3);
+            }
+            return vector;
+        }
+    }
+}
