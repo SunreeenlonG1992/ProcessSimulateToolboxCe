a044d03687a19af050f9cc460b4d121287cd45ca
markush <markus.hochholzer@robworld.de>
2020-03-01 10:30:06 +0100

    Add OpenMDD command

---
+++ Robworld.PsCommands/OpenMDD/RwOpenMDD.cs
@@ -0,0 +1,37 @@
+﻿using Robworld.PsPublicLibrary.Extensions;
+using Robworld.PsPublicLibrary.Utilities;
+using System.Diagnostics;
+using Tecnomatix.Engineering;
+
+namespace Robworld.PsCommands.OpenMDD
+{
+    /// <summary>
+    /// Open a machine data directory in explorer
+    /// </summary>
+    internal static class RwOpenMDD
+    {
+        /// <summary>
+        /// Opens the machine data directory of the selected robot
+        /// </summary>
+        internal static void OpenMachineDataDirectory()
+        {
+            TxObjectList selectedObjects = TxApplication.ActiveSelection.GetItems();
+            if (selectedObjects.Count == 1)
+            {
+                if (selectedObjects[0] is ITxRobot robot)
+                {
+                    string path = robot.GetMachineDataDirectory();
+
+                    if (RwFileAndDirectoryUtilities.DoesDirectoryExist(path))
+                    {
+                        Process.Start("explorer.exe", path);
+                    }
+                    else
+                    {
+                        throw new TxArgumentException($"The directory {path} does not exist!");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Robworld.PsCommands/OpenMDDCommand.cs
@@ -0,0 +1,116 @@
+﻿using Robworld.PsPublicLibrary.CommandEnablers;
+using Tecnomatix.Engineering;
+
+namespace Robworld.PsCommands
+{
+    /// <summary>
+    /// OpenMDD command class
+    /// </summary>
+    public class OpenMDDCommand : TxButtonCommand
+    {
+        #region Fields
+        private RwSingleRobotCommandEnabler enabler;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Get the Category under which the command appears
+        /// </summary>
+        public override string Category
+        {
+            get { return "Robworld GmbH & Co. KG"; }
+        }
+
+        /// <summary>
+        /// Get a short description of the command
+        /// </summary>
+        public override string Description
+        {
+            get { return "Open the machine data directory of the selected robot"; }
+        }
+
+        /// <summary>
+        /// Shows the tooltip of the command
+        /// </summary>
+        public override string Tooltip
+        {
+            get { return "Open the machine data directory of the selected robot"; }
+        }
+
+        /// <summary>
+        /// Get the name of the command
+        /// </summary>
+        public override string Name
+        {
+            get { return "Open MDD"; }
+        }
+
+        /// <summary>
+        /// Get the 16x16 bitmap of the command
+        /// </summary>
+        public override string Bitmap
+        {
+            get { return "Images.Commands.OpenMDD16x16.bmp"; }
+        }
+        
+        /// <summary>
+        /// Get the 32x32 bitmap of the command
+        /// </summary>
+        public override string LargeBitmap
+        {
+            get { return "Images.Commands.OpenMDD32x32.png"; }
+        }
+
+        /// <summary>
+        /// Get the command enabler
+        /// </summary>
+        public override ITxCommandEnabler CommandEnabler
+        {
+            get { return enabler; }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of the OpenMDD command
+        /// </summary>
+        public OpenMDDCommand()
+        {
+            enabler = new RwSingleRobotCommandEnabler();
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Executes the OpenMDD command
+        /// </summary>
+        /// <param name="cmdParams"></param>
+        public override void Execute(object cmdParams)
+        {
+            try
+            {
+                OpenMDD.RwOpenMDD.OpenMachineDataDirectory();
+            }
+            catch (TxArgumentException exception)
+            {
+                string caption = "OpenMDD argument exception!";
+                TxMessageBox.ShowModal(exception.Message, caption, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+            }
+            catch (System.Exception exception)
+            {
+                string caption = "OpenMDD exception!";
+                TxMessageBox.ShowModal(exception.Message, caption, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Exclamation);
+            }
+        }
+
+        /// <summary>
+        /// Activate the command if conditions are okay
+        /// </summary>
+        /// <returns>The boolean value of the conditions check</returns>
+        public override bool Connect()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
